50930b57249bc83f40bfebba4bdcbe4af6a8f6aa
boroksid@yandex.ru <boroksid@yandex.ru>
2023-01-08 20:53:26 +0300

    Добавьте файлы проекта.

---
+++ Modul_8_Task_2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace Modul_8_Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите полный адрес для папки");
+            string sourceDirectory = Console.ReadLine();
+            if (Directory.Exists(sourceDirectory))
+            {
+                var di = new DirectoryInfo(sourceDirectory);
+                DirSize(di);
+                Console.WriteLine("Размер папки : {0}", DirSize(di));
+            }
+        }
+        public static long DirSize(DirectoryInfo d)
+        {
+            long size = 0;
+            FileInfo[] fis = d.GetFiles();
+            foreach (FileInfo fi in fis)
+            {
+                size += fi.Length;
+            }
+
+            DirectoryInfo[] dis = d.GetDirectories();
+            foreach (DirectoryInfo di in dis)
+            {
+                size += DirSize(di);
+            }
+            return size;
+        }
+    }
+}
+
